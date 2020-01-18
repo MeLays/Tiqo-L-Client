b0eb748f2b49fc5626e21e0e341c9c15ff3d53db
Philipp S <seelos@outlook.com>
2020-01-18 13:46:27 +0100

    Added inobounce

--- TiqoLClient.js
+++ TiqoLClient.js
@@ -1,6 +1,12 @@
+//iNoBounce
+(function(global){var startY=0;var enabled=false;var supportsPassiveOption=false;try{var opts=Object.defineProperty({},"passive",{get:function(){supportsPassiveOption=true}});window.addEventListener("test",null,opts)}catch(e){}var handleTouchmove=function(evt){var el=evt.target;var zoom=window.innerWidth/window.document.documentElement.clientWidth;if(evt.touches.length>1||zoom!==1){return}while(el!==document.body&&el!==document){var style=window.getComputedStyle(el);if(!style){break}if(el.nodeName==="INPUT"&&el.getAttribute("type")==="range"){return}var scrolling=style.getPropertyValue("-webkit-overflow-scrolling");var overflowY=style.getPropertyValue("overflow-y");var height=parseInt(style.getPropertyValue("height"),10);var isScrollable=scrolling==="touch"&&(overflowY==="auto"||overflowY==="scroll");var canScroll=el.scrollHeight>el.offsetHeight;if(isScrollable&&canScroll){var curY=evt.touches?evt.touches[0].screenY:evt.screenY;var isAtTop=startY<=curY&&el.scrollTop===0;var isAtBottom=startY>=curY&&el.scrollHeight-el.scrollTop===height;if(isAtTop||isAtBottom){evt.preventDefault()}return}el=el.parentNode}evt.preventDefault()};var handleTouchstart=function(evt){startY=evt.touches?evt.touches[0].screenY:evt.screenY};var enable=function(){window.addEventListener("touchstart",handleTouchstart,supportsPassiveOption?{passive:false}:false);window.addEventListener("touchmove",handleTouchmove,supportsPassiveOption?{passive:false}:false);enabled=true};var disable=function(){window.removeEventListener("touchstart",handleTouchstart,false);window.removeEventListener("touchmove",handleTouchmove,false);enabled=false};var isEnabled=function(){return enabled};var testDiv=document.createElement("div");document.documentElement.appendChild(testDiv);testDiv.style.WebkitOverflowScrolling="touch";var scrollSupport="getComputedStyle"in window&&window.getComputedStyle(testDiv)["-webkit-overflow-scrolling"]==="touch";document.documentElement.removeChild(testDiv);if(scrollSupport){enable()}var iNoBounce={enable:enable,disable:disable,isEnabled:isEnabled};if(typeof module!=="undefined"&&module.exports){module.exports=iNoBounce}if(typeof global.define==="function"){(function(define){define("iNoBounce",[],function(){return iNoBounce})})(global.define)}else{global.iNoBounce=iNoBounce}})(this);
+
+iNoBounce.disable()
+
 /**
- * TiqoL Client JavaScriot indev version by Philipp Seelos
+ * TiqoL Client JavaScript 0.5 version by m-3.me
  */
+
 function getData(){
 	this.getVersion = function(){
 		return "0.5";
@@ -55,6 +61,14 @@ function TiqoLClient (server_adress , port){
 	console.log("   `--'   `--' `-|  | `---'        `-----'"); 
 	console.log("                 `--'");                      
 	console.log("Starting " + STATIC.getName() + " " + STATIC.getVersion());
+	
+		
+	//Send custom messages
+	this.sendCustomPaket = function(json){
+		custompaket = this.paketHandler.createPaket("c104" , secretKey , json);
+		console.log("Sending custom paket " + custompaket);
+		socket.send(custompaket);
+	}
 	
 	this.connect = function(){
 		console.log("Connecting to " + this.server_adress + ":" + this.port);
@@ -354,9 +368,11 @@ var HTMLObject = class{
 							ctx.closePath();
 							dot_flag = false;
 						}
+						iNoBounce.enable()
 					}
 					if (res == 'up' || res == "out") {
 						flag = false;
+						iNoBounce.disable()
 					}
 					if (res == 'move') {
 						if (flag) {
@@ -366,6 +382,7 @@ var HTMLObject = class{
 							currY = (e.clientY - canvas.getBoundingClientRect().top) / (canvas.clientHeight / canvas.height);
 							draw(curr);
 						}
+						iNoBounce.enable()
 					}
 				}
 			}
@@ -461,6 +478,7 @@ function HTMLBuilder(client){
 	}
 	
 	this.buildByJSON = function(array){
+		iNoBounce.disable()
 		topobject = createHTMLObject(array , client);
 		document.body = topobject.getElement();
 		direct_access = {};
@@ -512,7 +530,7 @@ function HTMLBuilder(client){
 	this.removeObject = function(id){
 		parent = searchParent(topobject,id);
 		parent.removeChild(searchObject(topobject,id))
-		console.log("HTML Object " + id + " removed by server")
+		console.log("HTML Object " + id + "removed by server")
 	}
 
 	this.addObjectToBody = function(array){
@@ -525,7 +543,6 @@ function HTMLBuilder(client){
 	this.updateObject = function(objectID , replaceObjectArray , keepOldChildren){
 
 		var position = document.scrollingElement.scrollTop;
-		console.log(position);
 		
 		replaceObject = createHTMLObject(replaceObjectArray , client);
 		oldObject = searchObject(topobject,objectID);
